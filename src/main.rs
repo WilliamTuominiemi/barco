8bfd4c27ebd178f97efcddf23a341b8c3611c413
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-10-29 23:05:49 +0200

    turn reading barcode to function

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
 use image::ImageReader;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut bc = ImageReader::open("barcode.png")?.decode()?;
-    let image_height = bc.height();
-    let image_width = bc.width();
-    let line = bc.crop(0, image_height / 2, image_width, 1);
+    let mut image = ImageReader::open("barcode.png")?.decode()?;
+    let image_height = image.height();
+    let image_width = image.width();
+    let cropped = image.crop(0, image_height / 2, image_width, 1);
+    let line = cropped.as_bytes();
 
-    let mut bytes = line.as_bytes().to_vec();
+    let barcode = read_barcode(line);
+    println!("{:?}", barcode);
+
+    Ok(())
+}
+
+fn read_barcode(line: &[u8]) -> Vec<u8> {
+    let mut bytes = line.to_vec();
 
     for i in 0..bytes.len() {
         let byte = bytes[i];
 
         if byte / 2 < 127 {
-            bytes[i] = 0;
+            bytes[i] = 1;
         } else {
-            bytes[i] = 255;
+            bytes[i] = 0;
         }
     }
 
     let mut start: Option<usize> = None;
     let mut end: Option<usize> = None;
     for i in 0..bytes.len() {
-        if bytes[i] == 0 && start.is_none() {
+        if bytes[i] == 1 && start.is_none() {
             start = Some(i);
-        } else if bytes[i] == 0 && start.is_some() {
+        } else if bytes[i] == 1 && start.is_some() {
             end = Some(i);
         }
     }
@@ -33,8 +41,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         _ => panic!("barcode not found"),
     };
 
-    println!("{:?}", bytes);
-    println!("{:?}", barcode);
-
-    Ok(())
+    barcode
 }
