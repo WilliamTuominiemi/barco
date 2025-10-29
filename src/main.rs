ed6d8c0fc16141d01d01b01160dcac790e6795d5
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-10-29 14:00:16 +0200

    read line from barcode as bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use image::ImageReader;
+use std::io::Cursor;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut bc = ImageReader::open("barcode.png")?.decode()?;
+    let image_height = bc.height();
+    let image_width = bc.width();
+    let line = bc.crop(0, image_height / 2, image_width, image_height);
+
+    let bytes = line.as_bytes();
+
+    println!("{:?}", bytes);
+
+    Ok(())
 }
