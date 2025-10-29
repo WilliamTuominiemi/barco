afe747429b38314685bdc51e9af75c78434e84e0
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-10-30 00:02:35 +0200

    read start guard

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,45 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let barcode = read_barcode(line);
     println!("{:?}", barcode);
+    let start_code_end_index = read_start_guard(barcode);
 
     Ok(())
+}
+
+fn read_start_guard(barcode: Vec<u8>) -> usize {
+    let mut indexes: Vec<usize> = vec![];
+    let mut lengths: Vec<usize> = vec![];
+
+    for i in 0..barcode.len() {
+        let byte = barcode[i];
+
+        let entries = indexes.len();
+        let last = if entries == 0 {
+            None
+        } else {
+            Some(indexes[entries - 1])
+        };
+
+        if (entries == 0 && byte == 0) || (entries == 1 && byte == 1) || (entries == 2 && byte == 0)
+        {
+            indexes.push(i - 1);
+
+            match last {
+                Some(index) => lengths.push(i - index - 1),
+                _ => lengths.push(i),
+            }
+        }
+    }
+
+    let ratio = lengths[0] as f64 / lengths[2] as f64;
+    if (ratio < 0.6) || (ratio > 1.4) {
+        panic!("Error reading start guard, invalid barcode")
+    }
+
+    println!("{:?}", indexes);
+    println!("{:?}", lengths);
+
+    indexes[2]
 }
 
 fn read_barcode(line: &[u8]) -> Vec<u8> {
@@ -19,7 +56,7 @@ fn read_barcode(line: &[u8]) -> Vec<u8> {
     for i in 0..bytes.len() {
         let byte = bytes[i];
 
-        if byte / 2 < 127 {
+        if byte / 2 < 100 {
             bytes[i] = 1;
         } else {
             bytes[i] = 0;
