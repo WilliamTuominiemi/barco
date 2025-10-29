fddef2cabd2f55af875d4018b1bf63d08fe0a6e6
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-10-29 22:05:10 +0200

    extract barcode

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use image::ImageReader;
-use std::io::Cursor;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut bc = ImageReader::open("barcode.png")?.decode()?;
@@ -7,9 +6,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let image_width = bc.width();
     let line = bc.crop(0, image_height / 2, image_width, 1);
 
-    let bytes = line.as_bytes();
+    let mut bytes = line.as_bytes().to_vec();
+
+    for i in 0..bytes.len() {
+        let byte = bytes[i];
+
+        if byte / 2 < 127 {
+            bytes[i] = 0;
+        } else {
+            bytes[i] = 255;
+        }
+    }
+
+    let mut start: Option<usize> = None;
+    let mut end: Option<usize> = None;
+    for i in 0..bytes.len() {
+        if bytes[i] == 0 && start.is_none() {
+            start = Some(i);
+        } else if bytes[i] == 0 && start.is_some() {
+            end = Some(i);
+        }
+    }
+
+    let barcode = match (start, end) {
+        (Some(s), Some(e)) => bytes[s..e].to_vec(),
+        _ => panic!("barcode not found"),
+    };
 
     println!("{:?}", bytes);
+    println!("{:?}", barcode);
 
     Ok(())
 }
