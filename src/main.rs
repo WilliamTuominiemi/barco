4ee947876fda6ac8b05fb0642ba5a17dac3d07fa
Tuominiemi William <william.tuominiemi@aalto.ficd>
2025-10-29 14:01:55 +0200

    pixel high line

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut bc = ImageReader::open("barcode.png")?.decode()?;
     let image_height = bc.height();
     let image_width = bc.width();
-    let line = bc.crop(0, image_height / 2, image_width, image_height);
+    let line = bc.crop(0, image_height / 2, image_width, 1);
 
     let bytes = line.as_bytes();
 
